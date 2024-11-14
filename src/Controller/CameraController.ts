dfc8cd0f28458eebe0609e69ce6b17d19b5afa2f
fisicagames <rafaelj3d@gmail.com>
2024-11-14 16:42:06 -0300

    chore(camera): adjust offset and target position in CameraController

--- src/Controller/CameraController.ts
+++ src/Controller/CameraController.ts
@@ -5,12 +5,12 @@ export class CameraController {
     // Método estático para atualizar a posição da câmera
     public static updatePosition(followCamera: FollowCamera, followCameraTarget: Mesh): void {
         if (followCamera && followCameraTarget) {
-            const offset = new Vector3(-15, 6, 0); 
+            const offset = new Vector3(-15, 4, 0); 
             const targetPosition = followCameraTarget.position.add(offset);
-            targetPosition.y = 4;
+            targetPosition.y = 3;
             followCamera.position = targetPosition;
 
-            const targetOffset = followCameraTarget.position.subtract(new Vector3(0, -3, 0));
+            const targetOffset = followCameraTarget.position.subtract(new Vector3(0, -1, 0));
             followCamera.setTarget(targetOffset); 
         }
     }
