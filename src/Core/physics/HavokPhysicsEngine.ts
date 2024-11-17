062d356cba015c3986866b87a5b7166afc7cd2bc
fisicagames <rafaelj3d@gmail.com>
2024-11-17 18:48:55 -0300

    style(HavokPhysicsEngine): Add gravity comment in line for search purpose.

--- src/Core/physics/HavokPhysicsEngine.ts
+++ src/Core/physics/HavokPhysicsEngine.ts
@@ -8,7 +8,7 @@ export class HavokPhysicsEngine   {
         });
 
         const hk = new HavokPlugin(true, havok);
-        scene.enablePhysics(new Vector3(0, -24.8, 0), hk);
+        scene.enablePhysics(new Vector3(0, -24.8, 0), hk); //gravity
         
         return hk;
     }
