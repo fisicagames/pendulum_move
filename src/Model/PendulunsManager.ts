b4c7ff1158d0a35d1a46d7c7a6d04c784fdca7ef
fisicagames <rafaelj3d@gmail.com>
2024-11-22 07:54:45 -0300

    refactor(PendulumsManager): Refactor by extract codes to new methods.

--- src/Model/PendulunsManager.ts
+++ src/Model/PendulunsManager.ts
@@ -14,71 +14,97 @@ export class PendulumsManager {
     constructor(scene: Scene) {
         this.scene = scene;
         this.pendulumsNode = new TransformNode("PendulumsNode", this.scene);
-        this.material = new StandardMaterial(`cylinderPendulumMassMaterial`, this.scene);
-        this.material.diffuseColor = new Color3(0.9, 0.1, 0.1);
-        this.material.reflectionTexture = null;
-        this.material.refractionTexture = null;
-        this.material.needDepthPrePass = false;        
+        this.material = this.createMaterial();
         this.initializePendulums();
     }
 
+    private createMaterial(): StandardMaterial {
+        const material = new StandardMaterial(`cylinderPendulumMassMaterial`, this.scene);
+        material.diffuseColor = new Color3(0.9, 0.1, 0.1);
+        material.reflectionTexture = null;
+        material.refractionTexture = null;
+        material.needDepthPrePass = false;
+        return material;
+    }
+
     public initializePendulums(): void {
-        this.totalScore = 0;
-        this.xDecrement = 20;
+        this.resetState();
         const pendulumPositions = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180];
         const yPosition = 20;
         pendulumPositions.forEach((xPosition) => {
-            this.pendulums.push(new Pendulum(this.scene, xPosition, yPosition, this.pendulumsNode, this.material));
+            this.addPendulum(xPosition, yPosition);
         });
     }
 
-    public updatePendulums(spherePlayerXPosition: number, spherePlayerYPosition: number, endGAme: boolean): void {
+    private resetState(): void {
+        this.totalScore = 0;
+        this.xDecrement = 20;
+        this.removeAllPendulums();
+    }
+
+    public updatePendulums(spherePlayerXPosition: number, spherePlayerYPosition: number, isGameOver: boolean): void {
         this.pendulums.forEach((pendulum, index) => {
             pendulum.adjustPendulumRodAngle();
-            if (!pendulum.hasPlayerScored){
-                if(!endGAme && spherePlayerXPosition > pendulum.xPosition &&
-                    spherePlayerYPosition < pendulum.yPosition){
-                    this.totalScore++;
-                    if (this.onScoreUpdatedCallback) {
-                        this.onScoreUpdatedCallback(this.totalScore);
-                    }                    
-                    pendulum.hasPlayerScored = true;
-                }
-            }
-            if (pendulum.xPosition < spherePlayerXPosition - this.xDecrement - 7) {
-                this.removePendulum(index);
-                const newYposition =  Math.round(pendulum.xPosition/300) % 2 === 0  ? 4.0: 20;
-                this.xDecrement = this.xDecrement > 2 ? this.xDecrement - 0.1: 2;                
-                //TODO: Remember to remove this console.log:
-                //console.log(this.xDecrement);
-                const newXposition = this.pendulums[this.pendulums.length-1].xPosition;
-                this.createNewPendulum(newXposition + this.xDecrement, newYposition);
-                
-            }
+            this.updateScore(pendulum, spherePlayerXPosition, spherePlayerYPosition, isGameOver);
+            this.handlePendulumPosition(pendulum, index, spherePlayerXPosition);
         });
     }
 
-    public getPendulums(): Pendulum[] {
-        return this.pendulums;
+    private updateScore(pendulum: Pendulum, spherePlayerX: number, spherePlayerY: number, isGameOver: boolean): void {
+        if (!pendulum.hasPlayerScored && !isGameOver &&
+            spherePlayerX > pendulum.xPosition && spherePlayerY < pendulum.yPosition) {
+            pendulum.hasPlayerScored = true;
+            this.incrementScore();
+        }
     }
-    public createNewPendulum(xPosition, yPosition){
-        this.pendulums.push(new Pendulum(this.scene, xPosition, yPosition, this.pendulumsNode, this.material));
+
+    private incrementScore(): void {
+        this.totalScore++;
+        if (this.onScoreUpdatedCallback) {
+            this.onScoreUpdatedCallback(this.totalScore);
+        }
+    }
+
+    private handlePendulumPosition(pendulum: Pendulum, index: number, spherePlayerX: number): void {
+        if (pendulum.xPosition < spherePlayerX - this.xDecrement - 7) {
+            this.removePendulum(index);
+            const newYPosition = this.calculateNewYPosition(pendulum.xPosition);
+            const newXPosition = this.getNextXPosition();
+            this.addPendulum(newXPosition, newYPosition);
+        }
+    }
+
+    private calculateNewYPosition(currentXPosition: number): number {
+        return Math.round(currentXPosition / 300) % 2 === 0 ? 4.0 : 20;
+    }
+
+    private getNextXPosition(): number {
+        this.xDecrement = Math.max(this.xDecrement - 0.1, 2);
+        return this.pendulums[this.pendulums.length - 1]?.xPosition + this.xDecrement || 0;
+    }
+
+    private addPendulum(xPosition: number, yPosition: number): void {
+        const newPendulum = new Pendulum(this.scene, xPosition, yPosition, this.pendulumsNode, this.material);
+        this.pendulums.push(newPendulum);
     }
 
     public removePendulum(index: number): void {
         if (index >= 0 && index < this.pendulums.length) {
-            const pendulum = this.pendulums[index];
-            pendulum.dispose(); // Chama o método dispose do pêndulo
-            this.pendulums.splice(index, 1); // Remove do array
+            this.pendulums[index].dispose();
+            this.pendulums.splice(index, 1);
         }
     }
-    
+
     public removeAllPendulums(): void {
-        this.pendulums.forEach(pendulum => pendulum.dispose()); // Dispose de todos
-        this.pendulums = []; // Limpa o array
+        this.pendulums.forEach(pendulum => pendulum.dispose());
+        this.pendulums = [];
     }
 
     public setOnScoreUpdatedCallback(callback: (newScore: number) => void): void {
         this.onScoreUpdatedCallback = callback;
+    }
+
+    public getPendulums(): Pendulum[] {
+        return this.pendulums;
     }
 }
